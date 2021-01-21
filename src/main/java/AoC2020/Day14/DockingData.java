a889fc52fbdda7a154b502d41541adc861d40454
SSobus <ssobus@gmail.com>
2021-01-20 21:59:36 -0800

    AoC2020 - Day 14 Part 1 Complete without Bit Manipulation... Refactor later

--- src/main/java/AoC2020/Day14/DockingData.java
+++ src/main/java/AoC2020/Day14/DockingData.java
@@ -20,31 +20,43 @@ public class DockingData {
         System.out.println("Part 1:");
         long start = System.nanoTime();
 
-        int answer = 0;
+        long answer = 0L;
 
         Map<Long, String> memory = new HashMap<>();
-        Long mask = 0L;
-        Long maskNot = 0L;
+        String mask = "";
 
         for (String instruction : instructions) {
-            String value = instruction.substring(instruction.indexOf("=") + 1).trim();
+            String rawString = instruction.substring(instruction.indexOf("=") + 1).trim();
 
             if (instruction.startsWith("mask")) {
-                mask = Long.parseLong(value.replace("X", "0"), 2);
-                maskNot = Long.parseLong(value.replace("X", "1"), 2);
+                mask = rawString;
                 continue;
             }
 
+            long value = Long.parseUnsignedLong(rawString);
+            String binaryStr = String.format("%36s", Long.toUnsignedString(value, 2)).replace(" ", "0");
+
+            //TODO: use bit manipulation - research and see how to do it with & | ^ ~
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < mask.length(); i++) {
+                char maskChar = mask.charAt(i);
+                if (maskChar == 'X') {
+                    result.append(binaryStr.charAt(i));
+                } else if (maskChar == '0' || maskChar == '1') {
+                    result.append(maskChar);
+                }
+            }
+
+//            System.out.println("raw : " + String.format("%d", value));
+//            System.out.println("val : " + String.format("%36s", binaryStr.replace(" ", "0")));
+//            System.out.println("mask: " + String.format("%36s", mask.replace(" ", "0")));
+//            System.out.println("res : " + String.format("%36s", result.toString().replace(" ", "0")));
             String mem = instruction.substring(instruction.indexOf("[") + 1, instruction.indexOf("]")).trim();
+            memory.put(Long.parseLong(mem), result.toString());
+        }
 
-            Long test = Long.parseLong(value);
-            String and = Long.toBinaryString(mask & test);
-            String or = Long.toBinaryString(mask | test);
-
-            System.out.println("mask: " + String.format("%35s", Long.toBinaryString(mask)).replace(" ", "0"));
-            System.out.println("val : " + String.format("%35s", Long.toBinaryString(test)).replace(" ", "0"));
-            System.out.println("and : " + String.format("%35s", and).replace(" ", "0"));
-            System.out.println("or  : " + String.format("%35s", or).replace(" ", "0"));
+        for (String val : memory.values()) {
+            answer += Long.parseUnsignedLong(val, 2);
         }
 
         System.out.println("Answer: " + answer);
